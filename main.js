b3afc2683bd281805ef476440fe2bd5fec9281e2
Akshiti <100438051+akshitithapliyal@users.noreply.github.com>
2022-06-14 19:21:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 song = "";
 scoreRightwrist = 0;
 scoreLeftwrist = 0;
-rightwrist_X = 0;
-rightwrist_Y = 0;
-leftwrist_X = 0;
-leftwrist_Y = 0;
+rightWrist_X = 0;
+rightWrist_Y = 0;
+leftWrist_X = 0;
+leftWrist_Y = 0;
 
 function preload(){
     song=loadSound("music.mp3");
@@ -30,10 +30,12 @@ function gotposes(results){
        
         scoreLeftwrist= results[0].pose.keypoints[9].score;
         console.log("rightwristscore = "+scoreRightwrist+" leftwristscore" + scoreLeftwrist);
-        rightwrist_Y= results[0].pose.rightwrist.y;
-        leftwrist_Y= results[0].pose.leftwrist.y;
-        console.log("rightwristY = "+rightwrist_Y);
-        console.log(" leftwristY = "+leftwrist_Y);
+        rightWrist_X= results[0].pose.rightWrist.x;
+        rightWrist_Y= results[0].pose.rightWrist.y;
+        leftWrist_X= results[0].pose.leftWrist.x;
+        leftWrist_Y= results[0].pose.leftWrist.y;
+        console.log("rightwristX = "+rightWrist_X+" rightwristY = "+rightWrist_Y);
+        console.log("leftwristX = "+leftWrist_X+" leftwristY = "+leftWrist_Y);
     }
 }
 
@@ -42,29 +44,29 @@ function draw(){
     fill(0, 320, 60);
     stroke(0, 30, 35);
     if(scoreLeftwrist>0.2){
-        circle(leftwrist_X,leftwrist_Y,20);
-        numlwy=Number(leftwrist_Y);
+        circle(leftWrist_X,leftWrist_Y,20);
+        numlwy=Number(leftWrist_Y);
         new_lwy=floor(numlwy * 2);
         lwy=new_lwy/1000;
         document.getElementById("volume").innerHTML="volume is "+ lwy;
         song.setVolume(lwy);
     }
     if(scoreRightwrist>0.2){
-        circle(rightwrist_X,rightwrist_Y,20);
-        if(rightwrist_Y>0&&rightwrist_Y<=100){
+        circle(rightWrist_X,rightWrist_Y,20);
+        if(rightWrist_Y>0&&rightWrist_Y<=100){
             document.getElementById("speed").innerHTML="speed is 0.5";
             song.rate(0.5);
-        } else if(rightwrist_Y>100&&rightwrist_Y<=200){
+        } else if(rightWrist_Y>100&&rightWrist_Y<=200){
             document.getElementById("speed").innerHTML="speed is 1x";
             song.rate(1);
     }
-    else if(rightwrist_Y>200&&rightwrist_Y<=300){
+    else if(rightWrist_Y>200&&rightWrist_Y<=300){
         document.getElementById("speed").innerHTML="speed is 1.5";
         song.rate(1.5);
-}else if(rightwrist_Y>300&&rightwrist_Y<=400){
+}else if(rightWrist_Y>300&&rightWrist_Y<=400){
     document.getElementById("speed").innerHTML="speed is 2";
     song.rate(2);
-}else if(rightwrist_Y>400&&rightwrist_Y<=500){
+}else if(rightWrist_Y>400&&rightWrist_Y<=500){
     document.getElementById("speed").innerHTML="speed is 2.5";
     song.rate(2.5);
 }
